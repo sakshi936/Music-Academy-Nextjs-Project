4168d68c5cd192e67856b9f04ab95910c02ae910
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-07-04 14:38:08 +0530

    fix: npm run build

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -22,7 +22,7 @@ function page() {
 			<h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-6 text-white">All Courses {CourseData.courses.length}</h1>
 			<div className="flex flex-wrap justify-center ">
 				{CourseData.courses.map((course: Courses) => (
-					<CardContainer className="inter-var mx-10">
+					<CardContainer className="inter-var mx-10" key={course.id}>
 						<CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
 							<CardItem translateZ="50" className="text-xl font-bold text-neutral-600 dark:text-white">
 								{course.title}
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,12 +5,13 @@ import { Button } from "./ui/moving-border";
 function HeroSection() {
 	return (
 		<div className="h-auto md:h-[40rem] w-full rounded-e-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 bg-gray-900">
-			<Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="blue" />
+			<Spotlight className=" left-60 -top-20" fill="blue" />
+			<Spotlight className=" left-30 -top-20" fill="purple" />
 			<div className="p-4 relative z-10  w-full text-center">
 				<h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the Art of Music </h1>
 				<p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-					Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true
-					potential.
+					Dive into our comprehensive music courses and transform your musical journey today &#46; Whether you&apos;re a beginner or looking to refine your skills&#44; join us to unlock
+					your true potential&#46;
 				</p>
 				<div className="mt-4">
 					<Link href={"/courses"}>
