2baa1d23118b0fbd386a3d2835ddddd83c5a5f58
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-07-04 15:13:43 +0530

    fix: about & spotlight

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,8 @@ function HeroSection() {
 	return (
 		<div className="h-auto md:h-[40rem] w-full rounded-e-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 bg-gray-900">
 			<Spotlight className=" left-60 -top-20" fill="blue" />
-			<Spotlight className=" left-30 -top-20" fill="purple" />
+			<Spotlight className=" left-30 -top-20 " fill="purple" />
+			<Spotlight className=" -left-32 -top-20" fill="blue" />
 			<div className="p-4 relative z-10  w-full text-center">
 				<h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the Art of Music </h1>
 				<p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
